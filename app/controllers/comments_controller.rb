cd43fcecb0a79127bc3d49c845e46a5042c86df2
bonbon0202 <bonbon0202@gmail.com>
2022-04-28 13:12:09 +0900

    feat: add comment feature

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,43 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[ show edit update destroy ]
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+
+    redirect_to post_path(@post)
+  end
+
+  def show
+  end
+
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_path(@post)
+  end
+
+  private
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:comment, :writer)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_27_235858) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_28_002300) do
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.string "writer"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -19,4 +28,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_235858) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
 end
